eb3b7f45c40ae8dd66b822a5b13b4d68d2da1a9b
dicaf <dicafre1404@gmail.com>
2024-11-10 21:10:35 +0100

    Colors + Font

--- app/src/main/java/com/example/prudentpouch/ui/theme/Color.kt
+++ app/src/main/java/com/example/prudentpouch/ui/theme/Color.kt
@@ -1,6 +1,7 @@
 package com.example.prudentpouch.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import androidx.core.graphics.toColorInt
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -9,3 +10,23 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//Brand Colors
+val PrussianBlue = Color(0xFF003049)
+val AirSuperiorityBlue = Color(0xFF669BBC)
+
+//StateColors
+val PictonBlue = Color(0xFF5DADE2) //INFO
+val TiffanyBlue = Color(0xFF94D2BD) //SUCCESS
+val AtomicTangerine = Color(0xFFFFA585) // WARNING
+val PantoneRed = Color(0xFFE63946) //ERROR
+
+//Neutral Colors
+val Night = Color(0xFF090A0C)
+val Jet = Color(0xFF272929)
+val OuterSpace = Color(0xFF464745)
+val DimGray = Color(0xFF646662)
+val BattelshipGrey = Color(0xFF82857F)
+val AshGray = Color(0xFFA0A39B)
+val AshLightGray = Color(0xFFBFC2B8)
+val Ivory = Color(0xFFFBFFF1)
--- app/src/main/java/com/example/prudentpouch/view/HomeScreen.kt
+++ app/src/main/java/com/example/prudentpouch/view/HomeScreen.kt
@@ -1,12 +1,18 @@
 package com.example.prudentpouch.view
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
 import com.example.prudentpouch.viewmodel.MyViewModel
 
 @Composable
-fun HomeScreen(navController: NavController, vm: MyViewModel) {
-    Box {
-    }
+fun HomeScreen(navController: NavController, vm: MyViewModel) {}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    val navController = rememberNavController()
+    HomeScreen(navController, vm = MyViewModel())
 }
